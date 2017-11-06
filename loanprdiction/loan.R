5c2c1d9c0693ac84a8fe5e2123e597e6de8b7723
meena sirisha145 <meenasirisha145@gmail.com>
2017-11-06 19:56:43 +0530

    k

--- loanprdiction/loan.R
+++ loanprdiction/loan.R
@@ -26,7 +26,76 @@ table(data$Credit_History)
 data$Credit_History[is.na(data$Credit_History)]=1
 table(data$Credit_History)
 
-data$Gender[data$Gender==' ' ]='M'
+data$Gender[data$Gender== ""]='M'
 apply(data,2, function(x) length(unique(x)))
+unique(data$Gender)
+
+unique(data$Self_Employed)
+data$Self_Employed[data$Self_Employed==""]="Yes"
+unique(data$Self_Employed)
 
 
+
+apply(data,2, function(x) length(unique(x)))
+attach(data)
+cols=c("Loan_Status","Married","Gender","Credit_History","Self_Employed")
+for (i in cols){
+  data[,i]=as.factor(data[,i])
+}
+set.seed(123)
+
+split = sample.split(data$Loan_Status, SplitRatio = 0.80)
+
+training = subset(data, split == TRUE)
+test = subset(data,split == FALSE)
+
+# Structure of the data
+str(training)
+attach(training)
+#model1=fastbw(lrm(Loan_Status~Application_ID+ Gender+ Married +Dependents  + Education +Self_Employed +ApplicantIncome
+ #                + CoapplicantIncome+LoanAmount  ),rule="p")
+#model1
+
+
+model=glm(Loan_Status~Application_ID+ Gender+ Married +Dependents  + Education +Self_Employed +ApplicantIncome
+          + CoapplicantIncome+LoanAmount  +Loan_Amount_Term +Credit_History +Property_Area  
+           ,data=training,family=binomial)
+summary(model)
+
+#removing property area
+model=glm(Loan_Status~Application_ID+ Gender+ Married +Dependents  + Education +Self_Employed +ApplicantIncome
+          + CoapplicantIncome+LoanAmount +Credit_History +Loan_Amount_Term   
+          ,data=training,family=binomial)
+summary(model)
+#removing loanamountterm
+model=glm(Loan_Status~Application_ID+ Gender+ Married +Dependents  + Education +Self_Employed +ApplicantIncome
+          + CoapplicantIncome+LoanAmount +Credit_History,data=training,family=binomial)
+summary(model)
+#removing credithistory
+model=glm(Loan_Status~Application_ID+ Gender+ Married +Dependents  + Education +Self_Employed +ApplicantIncome
+          + CoapplicantIncome+LoanAmount ,data=training,family=binomial)
+summary(model)
+#removing self employed
+model=glm(Loan_Status~Application_ID+ Gender+ Married +Dependents  + Education +ApplicantIncome
+          + CoapplicantIncome+LoanAmount ,data=training,family=binomial)
+summary(model)
+
+#removing married
+model=glm(Loan_Status~Application_ID+ Gender+Dependents  + Education +ApplicantIncome
+          + CoapplicantIncome+LoanAmount ,data=training,family=binomial)
+summary(model)
+
+#removing dependents
+model=glm(Loan_Status~Application_ID+ Gender+ Education +ApplicantIncome
+          + CoapplicantIncome+LoanAmount ,data=training,family=binomial)
+summary(model)
+
+#removing education
+model=glm(Loan_Status~Application_ID+ Gender+ ApplicantIncome
+          + CoapplicantIncome+LoanAmount ,data=training,family=binomial)
+summary(model)
+
+#removing gender
+model=glm(Loan_Status~Application_ID+  ApplicantIncome
+          + CoapplicantIncome+LoanAmount ,data=training,family=binomial)
+summary(model)
---
+++ melbournehouse/house.R
@@ -0,0 +1,25 @@
+library('ggplot2') # visualization
+library('ggthemes') # visualization
+library('scales') # visualization
+library('dplyr') # data manipulation
+library('caTools') # Spliting data
+library('rms') # For step wise regression
+
+getwd()
+setwd("F:/rWork/rProjects/rstatistics/melbournehouse")
+dataset=read.csv("Melbourne_house.csv",stringsAsFactors = F)
+dataset
+str(dataset)
+colSums(is.na(dataset))
+#replacing NA values of bedroom2 column
+table(dataset$Bedroom2)
+dataset$Bedroom2[is.na(dataset$Bedroom2)]=3
+table(dataset$Bedroom2)
+table(is.na(dataset$Bedroom2))
+
+#replacing NA values of Bathroom
+table(dataset$Bathroom)
+dataset$Bathroom[is.na(dataset$Bathroom)]=2
+table(dataset$Bathroom)
+
+#replacing NA values of Car
